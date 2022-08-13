bf506b142d506501d6f0a59f81d63864bc4fbdfa
khangzxrr@gmail.com <khangzxrr@gmail.com>
2022-08-13 13:27:49 +0700

    Add background worker TCP

--- Dieukhiendongco/Form1.Designer.cs
+++ Dieukhiendongco/Form1.Designer.cs
@@ -55,10 +55,10 @@ namespace Dieukhiendongco
             // 
             // Stop
             // 
-            this.Stop.Location = new System.Drawing.Point(97, 81);
-            this.Stop.Margin = new System.Windows.Forms.Padding(4);
+            this.Stop.Location = new System.Drawing.Point(146, 127);
+            this.Stop.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Stop.Name = "Stop";
-            this.Stop.Size = new System.Drawing.Size(100, 28);
+            this.Stop.Size = new System.Drawing.Size(150, 44);
             this.Stop.TabIndex = 26;
             this.Stop.Text = "Stop";
             this.Stop.UseVisualStyleBackColor = true;
@@ -66,10 +66,10 @@ namespace Dieukhiendongco
             // 
             // Start
             // 
-            this.Start.Location = new System.Drawing.Point(97, 49);
-            this.Start.Margin = new System.Windows.Forms.Padding(4);
+            this.Start.Location = new System.Drawing.Point(146, 77);
+            this.Start.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Start.Name = "Start";
-            this.Start.Size = new System.Drawing.Size(100, 28);
+            this.Start.Size = new System.Drawing.Size(150, 44);
             this.Start.TabIndex = 25;
             this.Start.Text = "Start";
             this.Start.UseVisualStyleBackColor = true;
@@ -78,96 +78,96 @@ namespace Dieukhiendongco
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(201, 138);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(302, 216);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(136, 17);
+            this.label6.Size = new System.Drawing.Size(203, 25);
             this.label6.TabIndex = 24;
             this.label6.Text = "current Speed (rpm)";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(345, 138);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(518, 216);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.Size = new System.Drawing.Size(36, 25);
             this.label1.TabIndex = 14;
             this.label1.Text = "00";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(513, 30);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(770, 47);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(25, 17);
+            this.label5.Size = new System.Drawing.Size(38, 25);
             this.label5.TabIndex = 23;
             this.label5.Text = "kD";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(428, 30);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(642, 47);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(18, 17);
+            this.label4.Size = new System.Drawing.Size(28, 25);
             this.label4.TabIndex = 22;
             this.label4.Text = "kI";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(342, 30);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(513, 47);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.Size = new System.Drawing.Size(37, 25);
             this.label3.TabIndex = 21;
             this.label3.Text = "kP";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(201, 30);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(302, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.Size = new System.Drawing.Size(112, 25);
             this.label2.TabIndex = 20;
             this.label2.Text = "Set Speed";
             // 
             // kD
             // 
-            this.kD.Location = new System.Drawing.Point(517, 49);
-            this.kD.Margin = new System.Windows.Forms.Padding(4);
+            this.kD.Location = new System.Drawing.Point(776, 77);
+            this.kD.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.kD.Name = "kD";
-            this.kD.Size = new System.Drawing.Size(76, 22);
+            this.kD.Size = new System.Drawing.Size(112, 31);
             this.kD.TabIndex = 19;
             this.kD.Text = "0.03";
             // 
             // kI
             // 
-            this.kI.Location = new System.Drawing.Point(432, 49);
-            this.kI.Margin = new System.Windows.Forms.Padding(4);
+            this.kI.Location = new System.Drawing.Point(648, 77);
+            this.kI.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.kI.Name = "kI";
-            this.kI.Size = new System.Drawing.Size(76, 22);
+            this.kI.Size = new System.Drawing.Size(112, 31);
             this.kI.TabIndex = 18;
             this.kI.Text = "0.05";
             // 
             // kP
             // 
-            this.kP.Location = new System.Drawing.Point(346, 49);
-            this.kP.Margin = new System.Windows.Forms.Padding(4);
+            this.kP.Location = new System.Drawing.Point(519, 77);
+            this.kP.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.kP.Name = "kP";
-            this.kP.Size = new System.Drawing.Size(76, 22);
+            this.kP.Size = new System.Drawing.Size(112, 31);
             this.kP.TabIndex = 17;
             this.kP.Text = "0.07";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(205, 81);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(308, 127);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(150, 44);
             this.button1.TabIndex = 16;
             this.button1.Text = "send";
             this.button1.UseVisualStyleBackColor = true;
@@ -175,10 +175,10 @@ namespace Dieukhiendongco
             // 
             // SetSpeed
             // 
-            this.SetSpeed.Location = new System.Drawing.Point(205, 49);
-            this.SetSpeed.Margin = new System.Windows.Forms.Padding(4);
+            this.SetSpeed.Location = new System.Drawing.Point(308, 77);
+            this.SetSpeed.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.SetSpeed.Name = "SetSpeed";
-            this.SetSpeed.Size = new System.Drawing.Size(132, 22);
+            this.SetSpeed.Size = new System.Drawing.Size(196, 31);
             this.SetSpeed.TabIndex = 15;
             this.SetSpeed.Text = "0";
             // 
@@ -188,14 +188,15 @@ namespace Dieukhiendongco
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(2, 175);
+            this.chart1.Location = new System.Drawing.Point(3, 273);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series1.Legend = "Legend1";
             series1.Name = "Speed";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(792, 263);
+            this.chart1.Size = new System.Drawing.Size(1188, 411);
             this.chart1.TabIndex = 27;
             this.chart1.Text = "chart1";
             // 
@@ -206,18 +207,23 @@ namespace Dieukhiendongco
             // TCP_receive
             // 
             this.TCP_receive.AutoSize = true;
-            this.TCP_receive.Location = new System.Drawing.Point(596, 137);
+            this.TCP_receive.Location = new System.Drawing.Point(894, 214);
+            this.TCP_receive.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TCP_receive.Name = "TCP_receive";
-            this.TCP_receive.Size = new System.Drawing.Size(97, 17);
+            this.TCP_receive.Size = new System.Drawing.Size(147, 25);
             this.TCP_receive.TabIndex = 28;
             this.TCP_receive.Text = "Data_Receive";
             this.TCP_receive.Click += new System.EventHandler(this.TCP_receive_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 703);
             this.Controls.Add(this.TCP_receive);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.Stop);
@@ -233,6 +239,7 @@ namespace Dieukhiendongco
             this.Controls.Add(this.kP);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.SetSpeed);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_onClosing);
--- Dieukhiendongco/Form1.cs
+++ Dieukhiendongco/Form1.cs
@@ -17,13 +17,8 @@ namespace Dieukhiendongco
     {
         int iteration;
         string speed;
-        Int32 port = 8081;
-        IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-        TcpListener server = null;
-        Byte[] bytes = new Byte[256];
+       
         String data = null;
-
-
 
 
         public Form1()
@@ -34,37 +29,66 @@ namespace Dieukhiendongco
         private void Form1_Load(object sender, EventArgs e)
         {
 
-
-            // Set the TcpListener on port 13000.
-
-
-
-            // TcpListener server = new TcpListener(port);
-            server = new TcpListener(localAddr, port);
-
-            // Start listening for client requests.
-            server.Start();
-
-            // Buffer for reading data
+            var backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerAsync();
 
 
             timer1.Enabled = true;
 
 
-            serialPort.DtrEnable = true;
-            //khi arduino giao tiep qua USB, thi DTR phai duoc bat thi ham datareceived moi duoc goi.
+            //serialPort.DtrEnable = true;
+            ////khi arduino giao tiep qua USB, thi DTR phai duoc bat thi ham datareceived moi duoc goi.
 
-            serialPort.Open();
+            //serialPort.Open();
 
-            if (serialPort.IsOpen)
+            //if (serialPort.IsOpen)
+            //{
+            //    Console.WriteLine("Port is opened");
+            //}
+
+            //serialPort.DataReceived += SerialPort_DataReceived;
+            ////binding event
+
+            //iteration = 300;
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Int32 port = 8081;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests.
+            server.Start();
+
+            Console.WriteLine("init TCP");
+
+            Byte[] bytes = new Byte[256];
+
+            while (true)
             {
-                Console.WriteLine("Port is opened");
+                Console.WriteLine("Waiting for a connection... ");
+
+                // Perform a blocking call to accept requests.
+                // You could also use server.AcceptSocket() here.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                var stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = Encoding.ASCII.GetString(bytes, 0, i);
+                    //Console.WriteLine("Received: {0}", data);
+
+                }
+
             }
-
-            serialPort.DataReceived += SerialPort_DataReceived;
-            //binding event
-
-            iteration = 300;
         }
 
         private void SerialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
@@ -117,7 +141,7 @@ namespace Dieukhiendongco
         {
             Console.WriteLine("CLosing serial port");
             serialPort.Close();
-            server.Stop();
+           
 
         }
 
@@ -128,26 +152,14 @@ namespace Dieukhiendongco
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Console.WriteLine("Waiting for a connection... ");
-
-            //NetworkStream stream = client.GetStream();
-            //data = null;
-            //int i;
-            //while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-            //{
-            // Translate data bytes to a ASCII string.
-            // data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-            // Console.WriteLine("Received: {0}", data);
-
-            // Process the data sent by the client.
+            //data
+            if (data == null || data.Length == 0) 
+            {
+                return;
+            }
 
 
-            // Send back a response.
-            //}
-            //client.Close();
-
-
-
+            Console.WriteLine(data);
         }
     }
 }
